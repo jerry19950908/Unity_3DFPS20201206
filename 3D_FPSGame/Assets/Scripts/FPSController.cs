2d48e9d1ae3fcd688de7fc8348954607492bafc6
jerry19950908 <57181340+jerry19950908@users.noreply.github.com>
2021-02-21 03:23:39 +0800

    完成FPS基本場景

---
+++ 3D_FPSGame/Assets/Scripts/FPSController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 2000)]
+    public float speed;
+    [Header("旋轉"), Range(0, 2000)]
+    public float turn;
+
+    private Animator ani;
+    private Rigidbody rig;
+
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    /// <summary>
+    /// 移動方法
+    /// </summary>
+    private void Move()
+    {
+        float v = Input.GetAxis("Vertical");     // W S 上 下 - 前進值 = 1  後退值 = -1
+        float h = Input.GetAxis("Horizontal");   // A D 左 右
+
+        rig.MovePosition(transform.position + transform.forward * v * speed * Time.deltaTime + transform.right * h * speed * Time.deltaTime);
+    }
+}
